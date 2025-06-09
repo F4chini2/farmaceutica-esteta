966fcca08fa2184719148d83e2e67a32e8805659
F4chini2 <ens-thiagofachini@ugv.edu.br>
2025-06-09 17:40:10 -0300

    Removido pool extra

--- backend/middleware/admin.js
+++
@@ -1,9 +0,0 @@
-const adminOnly = (req, res, next) => {
-  if (req.usuario && req.usuario.tipo === 'admin') {
-    return next(); // Libera o acesso
-  }
-
-  return res.status(403).json({ erro: 'Acesso restrito a administradores' });
-};
-
-module.exports = adminOnly;
