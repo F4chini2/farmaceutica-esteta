787ead5b584bf62faaaebcd2820c2f530bcceedb
F4chini2 <ens-thiagofachini@ugv.edu.br>
2025-10-10 00:57:35 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'tccuser',
-  host: 'localhost',
-  database: 'tccdb',
-  password: 'tccpass',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
