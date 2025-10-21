e452e708d863e702643d36d1d0bd0ca1287b2b6f
F4chini2 <ens-thiagofachini@ugv.edu.br>
2025-10-21 13:22:32 -0300

    Update ClientesFull.jsx

--- frontend/src/pages/ClientesFull.jsx
+++ frontend/src/pages/ClientesFull.jsx
@@ -6,7 +6,7 @@ import { API, authHeaders } from '../config/api';
 function ClientesFull() {
   const navigate = useNavigate();
   const [form, setForm] = useState({
-    nome: '', telefone: '', alergias: '', descricao: '', idade: '', cpf: '', endereco: '', instagram: '',
+    nome: '', telefone: '', cpf: '', descricao: '', idade: '', alergias: '', endereco: '', instagram: '',
     motivo_avaliacao: '', tratamento_anterior: '', alergia_medicamento: '', uso_medicamento: '',
     usa_filtro_solar: 'false', usa_acido_peeling: 'false', problema_pele: '', gravida: 'false',
     cor_pele: '', biotipo_pele: '', hidratacao: '', acne: '',
